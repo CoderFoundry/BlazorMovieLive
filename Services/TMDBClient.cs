21e9892dbe034426debfd6626b9631cfdc540fe3
Jacob Visick <jacobvisick@gmail.com>
2023-11-13 19:29:49 -0500

    Add pagination

--- Services/TMDBClient.cs
+++ Services/TMDBClient.cs
@@ -18,9 +18,12 @@ namespace BlazorMovieLive.Services
             _httpClient.DefaultRequestHeaders.Authorization = new("Bearer", apiKey);
         }
 
-        public Task<PopularMoviePagedResponse?> GetPopularMoviesAsync()
+        public Task<PopularMoviePagedResponse?> GetPopularMoviesAsync(int page = 1)
         {
-            return _httpClient.GetFromJsonAsync<PopularMoviePagedResponse>("movie/popular");
+            if (page < 1) page = 1;
+            if (page > 500) page = 500;
+
+            return _httpClient.GetFromJsonAsync<PopularMoviePagedResponse>($"movie/popular?page={page}");
         }
 
         public Task<MovieDetails?> GetMovieDetailsAsync(int id)
